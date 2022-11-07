55502bfa8a6dc020d9b41cbc3d0803eb3e1d7b1a
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-11-07 12:06:01 -0500

    Add domain entities

---
+++ src/main/java/com/example/hexagonal/domain/Orders.java
@@ -0,0 +1,10 @@
+package com.example.hexagonal.domain;
+
+import java.math.BigDecimal;
+
+public class Orders {
+    private String id;
+    private String customerId;
+    private BigDecimal total;
+
+}
